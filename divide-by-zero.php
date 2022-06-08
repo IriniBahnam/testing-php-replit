c9afe32930faefcd4e14bcdd070906aaff09d20b
IriniBahnam <103008748+IriniBahnam@users.noreply.github.com>
2022-06-08 09:37:37 -0700

    Update divide-by-zero.php

--- divide-by-zero.php
+++ divide-by-zero.php
@@ -2,7 +2,8 @@
 //divide-by-zero.php
 require_once 'simpletest/autorun.php';
 include 'Calculator.php';
-	
+
+class TestOfCalculator extends UnitTestCase {
 	function testDivide() {
 	  $myCalc = new Calculator();
 	  $this->assertEqual($myCalc->divide(3,0), "ERR", "divide by zero, returns ERR");
