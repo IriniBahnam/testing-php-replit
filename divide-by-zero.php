265436e8a561257d6a9df2ad3acb6a4eababe162
IriniBahnam <103008748+IriniBahnam@users.noreply.github.com>
2022-06-08 09:35:08 -0700

    Create divide-by-zero.php

---
+++ divide-by-zero.php
@@ -0,0 +1,10 @@
+<?php
+//divide-by-zero.php
+require_once 'simpletest/autorun.php';
+include 'Calculator.php';
+	
+	function testDivide() {
+	  $myCalc = new Calculator();
+	  $this->assertEqual($myCalc->divide(3,0), "ERR", "divide by zero, returns ERR");
+	}
+}
